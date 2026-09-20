193b49a14655e888ba08744fffff86f1bc8d0540
Nawadays-ai <giantohebatt@gmail.com>
2026-09-20 17:32:17 +0700

    debugging and adding minimalist ui

--- app/src/main/java/com/example/screentranslator/SettingsActivity.kt
+++ app/src/main/java/com/example/screentranslator/SettingsActivity.kt
@@ -7,6 +7,7 @@ import android.text.TextWatcher
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
